6dee4ca4dafddc537cdf6383bc105881fe4f07d8
Jianmei <Jianmei@shijianmeideiMac.local>
2017-08-31 16:36:00 +0800

    first commit

--- lockDemo/ViewController.m
+++ lockDemo/ViewController.m
@@ -9,14 +9,51 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic,assign)int tickets;
+@property (nonatomic,strong)dispatch_queue_t  concurrentQueue;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+}
+#pragma mark -@synchronized 关键字加锁 互斥锁，性能较差不推荐使用
+-(void)testSynchronized
+{
+    self.concurrentQueue = dispatch_queue_create(0, 0);
+    //设置票的数量为5
+    _tickets = 5;
+    
+    //线程1
+    dispatch_async(self.concurrentQueue, ^{
+        [self saleTickets];
+    });
+    
+    //线程2
+    dispatch_async(self.concurrentQueue, ^{
+        [self saleTickets];
+    });
+    
+
+}
+
+- (void)saleTickets
+{
+    while (1) {
+        @synchronized(self) {
+            [NSThread sleepForTimeInterval:1];
+            if (_tickets > 0) {
+                _tickets--;
+                NSLog(@"剩余票数= %d, Thread:%@",_tickets,[NSThread currentThread]);
+            } else {
+                NSLog(@"票卖完了  Thread:%@",[NSThread currentThread]);
+                break;
+            }
+        }
+    }
 }
 
 
